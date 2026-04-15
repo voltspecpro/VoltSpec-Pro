35bf1dd2a9e086c385667db430122d3f14d9a036
Volt Spec Pro <voltspecpro@gmail.com>
2026-04-14 21:39:43 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -525,7 +525,7 @@ elif aba == "🏠 Cargas":
 
 
     # --- MÓDULO Luminotecnica  ---
-    elif aba== "💡 Luminotecnica":
+    if aba== "💡 Luminotecnica":
             st.header("💡 Dimensionamento Luminotécnico (NBR ISO/CIE 8995-1)")
             st.info("Este módulo utiliza o Método dos Lúmens para calcular a quantidade de luminárias necessária.")
 
